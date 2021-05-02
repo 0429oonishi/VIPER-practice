7e22ed93fe4d4541835d5c28c8f9492c431d9653
0429oonishi <oonishireon110@gmail.com>
2021-05-02 13:36:36 +0900

    不要な実装を消去

--- VIPER-practice/Utility/GitHubAPI.swift
+++ VIPER-practice/Utility/GitHubAPI.swift
@@ -43,38 +43,3 @@ struct GitHubSearchParameters {
     }
     
 }
-
-protocol GitHubAPIProtocol: AnyObject {
-    func get(parameters: GitHubSearchParameters,
-             handler: ResultHandler<[GitHubSearch]>?)
-}
-
-final class GitHubAPI: GitHubAPIProtocol {
-    
-    static let shared = GitHubAPI()
-    private init() { }
-    
-    func get(parameters: GitHubSearchParameters,
-             handler: ResultHandler<[GitHubSearch]>? = nil) {
-        guard parameters.validation else {
-            handler?(.failure(.error))
-            return
-        }
-        guard let url = URL(string: "https://api.github.com/search/repositories?\(parameters.queryParameter)") else {
-            handler?(.failure(.invalidUrl))
-            return
-        }
-        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
-            guard let data = data,
-                  let gitHubResponse = try? JSONDecoder().decode(GitHubResponse.self, from: data),
-                  let items = gitHubResponse.items else {
-                handler?(.failure(.error))
-                return
-            }
-            handler?(.success(items))
-        }
-        task.resume()
-    }
-    
-}
-
